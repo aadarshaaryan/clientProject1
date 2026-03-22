089f3cfb6e123ff44f936d019773e0b912cc544d
aadarshaaryan <aadarshaaryan84@gmail.com>
2026-03-23 01:07:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById("menu");
 const sideBar = document.getElementById("sideBar");
 
-menu.addEventListener("click", (e) => {
+menu.addEventListener("click", () => {
     sideBar.classList.toggle("open");
 });
