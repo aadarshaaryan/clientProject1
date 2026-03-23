226a4f5b444555046d17c05d3607fcd51106ceb5
aadarshaaryan <aadarshaaryan84@gmail.com>
2026-03-23 13:28:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const menu = document.getElementById("menu");
 const sideBar = document.getElementById("sideBar");
+const cross = document.getElementById("cross");
+
 
 menu.addEventListener("click", () => {
+    sideBar.classList.toggle("open");
+});
+
+cross.addEventListener("click", () => {
     sideBar.classList.toggle("open");
 });
